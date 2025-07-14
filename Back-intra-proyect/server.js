faddd7d43066f0b1ec11d3963a28513f64f624ba
JulianAmado-dev <guansomer@gmail.com>
2025-07-14 10:09:56 -0500

    Cambios en el server para despliegue en prod

--- Back-intra-proyect/server.js
+++ Back-intra-proyect/server.js
@@ -97,7 +97,7 @@ const upload = multer({
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://localhost:3000"],
     credentials: true,
   })
 );
